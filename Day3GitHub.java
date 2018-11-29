b25a7ba2a5ff78ecd1e23a4c6f53fe6607bd0978
Nisarga Shashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2018-11-29 09:40:45 -0500

    Update Day3GitHub.java
    Adds a println statement to the main method.

--- Day3GitHub.java
+++ Day3GitHub.java
@@ -2,5 +2,6 @@ public class Day3GitHub{
 	public static void main(String[]args){
 		System.out.println("HI!");
 		System.out.println("Ria");
+		System.out.println("Partner A: NisargaShashidhar");
 	}
 }
